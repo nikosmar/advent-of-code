f82cde565c7a2ab6f9bd4737dac59cfcc20b293d
nikos <soadnick@outlook.com>
2021-12-08 02:26:07 +0200

    robust day07 solutions

--- 07/07.rs
+++ 07/07.rs
@@ -1,20 +1,6 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-fn median(number_vector: &mut Vec<i32>) -> f32 {
-    number_vector.sort_unstable();
-
-    let length = number_vector.len() as f32;
-    let left_median = number_vector[(length / 2.0).floor() as usize] as f32;
-    let right_median = number_vector[(length / 2.0).ceil() as usize] as f32;
-
-    if left_median == right_median {
-        left_median
-    } else {
-        (left_median + right_median) / 2.0
-    }
-}
-
 fn main() {
     let file = File::open("crabs.txt").unwrap();
     let reader = BufReader::new(file);
@@ -29,24 +15,26 @@ fn main() {
         crabs_sum += crab_pos;
     }
 
-    let amount_of_crabs = crabs_positions.len() as i32;
-    let median = median(&mut crabs_positions) as i32;
+    crabs_positions.sort_unstable();
+    let amount_of_crabs = crabs_positions.len() as f32;
 
-    // integer division done on purpose
-    let mut mean = crabs_sum / amount_of_crabs;
-    // by now crabs_positions is sorted, therefore we can binary search it
-    match crabs_positions.binary_search(&mean) {
-        Ok(..) => mean += 1,
-        Err(..) => (),
-    }
+    let median = crabs_positions[(amount_of_crabs / 2.0) as usize];
+    let mean_floor = (crabs_sum as f32 / amount_of_crabs).floor() as i32;
+    let mean_ceil = (crabs_sum as f32 / amount_of_crabs).ceil() as i32;
 
-    let mut fuel_sum = (0, 0);
+    let mut fuel_sum = (0, 0, 0);
     for crab in crabs_positions {
-        let dist = (mean - crab).abs();
-
         fuel_sum.0 += (median - crab).abs();
-        fuel_sum.1 += (dist * (dist + 1)) / 2;
+
+        let dist_floor = (mean_floor - crab).abs();
+        let dist_ceil = (mean_ceil - crab).abs();
+
+        fuel_sum.1 += (dist_floor * (dist_floor + 1)) / 2;
+        fuel_sum.2 += (dist_ceil * (dist_ceil + 1)) / 2;
     }
 
+    if fuel_sum.2 < fuel_sum.1 {
+        fuel_sum.1 = fuel_sum.2;
+    }
     println!("Part One: {}\nPart Two: {}", fuel_sum.0, fuel_sum.1);
 }
